326aa17cff9dfe960c3fa6bcb869ef1a96e1cdac
kei1115 <keifukuda1115@gmail.com>
2021-11-28 06:29:11 +0900

    Test

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def get_response_message(mes_from):
                 try:
                     cur.execute('SELECT * FROM sample')
                     rows = cur.fetchall()
-                    return rows
+                    return cur.exists("sample")
                 except:
                     mes = "exception"
                     return mes
