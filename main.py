6a181eb2ace780fe0b2078b4b1473c0e7fb27513
kei1115 <keifukuda1115@gmail.com>
2021-12-04 08:43:55 +0900

    test

--- main.py
+++ main.py
@@ -147,8 +147,7 @@ def handle_message(event):
             with conn.cursor() as cur:
                 try:
                     print('aaa')
-                    cur.execute('update users set program=' + num + 'where id=' + '\'' + event.source.user_id + '\'')
-                    cur.execute('SELECT * from product;')
+                    cur.execute('update users set program=2 where id=' + '\'' + event.source.user_id + '\'')
                     a = cur.fetchone()
                     print("why?")
                     return a
